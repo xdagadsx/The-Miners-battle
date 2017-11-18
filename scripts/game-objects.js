ef62b1be54f429f8168f87c528eb2ed7e12e03a5
xdagadsx <Wiktor.Gorka@ad.cortex.uk>
2017-11-18 22:19:30 +0100

    preventing from default space and arrows behavior

--- scripts/game-objects.js
+++ scripts/game-objects.js
@@ -13,6 +13,10 @@
         var faceDirection = directions.S;
 
         addEventListener('keydown', (event) => {
+            if ([32, 37, 38, 39, 40].indexOf(event.keyCode) > -1) {
+                event.preventDefault();
+            }
+
             switch (event.code) {
                 case 'ArrowUp':
                     pressedKeys.ArrowUp = true;
@@ -29,7 +33,7 @@
                 case 'Space':
                     pressedKeys.Space = true;
             }
-        });
+        }, false);
         addEventListener('keyup', (event) => {
             switch (event.code) {
                 case 'ArrowUp':
@@ -45,7 +49,7 @@
                     pressedKeys.ArrowLeft = false;
                     break;
             }
-        });
+        }, false);
 
         var that = this;
         that.identifier = 'player';
